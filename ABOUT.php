bf81aa50bec234bd3ea8098321f71803d3c262e1
fadloulah <85185556+fadloulah@users.noreply.github.com>
2021-06-01 16:44:22 +0000

    about

---
+++ ABOUT.php
@@ -0,0 +1,49 @@
+<?php include('index.php');?>
+
+<?php  
+include("connexion_db/conn_db.php");?>
+  <!doctype html> 
+
+<html> 
+	<head>
+		<meta charset="utf-8">
+		<title> Ma page</title>
+	</head>
+	<body > 
+		<center>
+			<table border="1" width="500" height="500">
+				
+				<tr>
+					<td rowspan="2">
+						<ol type="I">
+							<li>WEB FONDAMENTALE</li>
+								<ol type="1">
+									<li>les bases du web</li>
+								</ol>
+							<li>HTML</li>
+								<ol type="a">
+									<li>les bases</li>
+								</ol>
+						</ol>
+					</td>
+					
+				</tr>
+				
+				<tr>
+					
+					<td colspan="2">
+						<embed src="birahime.mp3" />
+					</td>
+					
+					<td>MERCI</td>
+				</tr>
+				
+				<tr>
+					<td><img src="B.jpg" width="100"/></td>
+					<td>NGOM Serigne Fallou</td>
+					<td><img src="B.jpg" width="100"/></td>
+					<td><a href="Connexion.html">Retour</a></td>
+				</tr>	
+		</center>
+	</body>
+</html>
