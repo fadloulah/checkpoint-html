141204ed4aa4150e646c7cf9137cea1124e0e1d2
fadloulah <85185556+fadloulah@users.noreply.github.com>
2021-06-01 16:45:15 +0000

    index

---
+++ index.php
@@ -0,0 +1,108 @@
+ <!doctype html>
+<html>
+	<head>
+		<meta charset="UTF.8">
+		<title> CHECKPOINT HTML</title>
+		<style>
+body {
+  background-image: url("ps.JPG");
+  background-repeat: no-repeat;
+  background-size: 100%;
+}
+td{
+	color: green;
+}
+header
+ {
+	 
+	 height: 150px;
+	 text-align: right;
+	 padding-right:35px;
+	 padding-left:35px;
+	 margin-bottom: 20px;
+	 box-shadow: 5px 5px 12px black;
+	 border-radius: 50px;
+	 background: -webkit-linear-gradient(top, pink, yellow);
+	 border: red;	 
+ }
+ header, a
+ {
+	text-decoration: none;
+	color: black;
+	
+ }
+ a:hover
+ {
+	 color: red;
+	 text-decoration: underline;
+ }
+  nav
+ {
+	 border: 1px solid black;
+	 
+	 height: 45px;
+	 padding-top: 5px;
+	 box-shadow: 5px 5px 20px black;
+	 text-shadow: 2px 2px 12px white;
+	 border-radius: 10px;
+	 background: -webkit-linear-gradient(top, pink, white); 
+ } 
+ footer{
+ 	 border: 1px solid black;
+ 	 height: 45px;
+	 padding-top: 5px;
+	 box-shadow: 5px 5px 20px black;
+	 text-shadow: 2px 2px 12px white;
+	 border-radius: 10px;
+	 background: -webkit-linear-gradient(top, pink, white); 
+	 position: relative;
+   bottom:-630px;
+   top:px; 
+ }
+</style>
+	</head>
+	<body>
+		<header>
+			<table class="tabb" width="40px" align="right">
+				<br>
+				<font color="black"><h1><marquee>CHECKPOINT HTML</marquee></h1></font>	
+				</table>
+		</header>
+		<center>
+				<nav>
+			<center>
+					<table border="0" width=500 height="30">
+						<tr>
+							<th>
+								<a href="index.php"> ACCEUIL </a>
+							</th>
+							<th>
+								<a href="form_inscription.php"> HOME </a>
+
+							</th>
+							<th>
+								<a href="ABOUT.php"> ABOUT </a>
+
+							</th>
+							<th>
+								<a href="WORK.php">WORK</a>
+								
+							</th>
+							<th>
+								<a href="RESUME.php"> RESUME</a>
+
+							</th>
+							<th>
+								<a href="CONTACT.php">CONTACT</a>
+							</th>
+							
+						</tr>
+					</table>
+			</center>
+			</nav>
+		</center>
+		<footer>
+			<font color="black"><h3><marquee> COPYRIGHT 2020 CHEIKH MOUHAMADOUL FADAL</marquee></h3></font>
+		</footer>
+	</body>
+</html>
